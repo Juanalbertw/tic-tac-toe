25c8a7ba2b7ccc6b50c716c15be0ba7e94c46ad6
Juan Albert Wibowo <juanalbert240303@gmail.com>
2021-07-30 10:37:46 +0700

    Add dark mode and winner announcement

--- js/GameView.js
+++ js/GameView.js
@@ -1,37 +1,86 @@
 export default class GameView {
-
   constructor() {
-    console.log("init game view")
+    console.log("init game view");
   }
 
-  updateBoard(game){
-    this.updateTurn(game);
+  updateBoard(game, mode) {
+    this.updateTurn(game, mode);
     const winningCombination = game.findWinningCombinations();
-    for(let i = 0; i < game.board.length; i++){
-      const tile = document.querySelector(`.board-tile[data-index='${i}']`);
-      
-      tile.classList.remove("tile-winner");
-      
-      let tileType = game.board[i] == 'X' ? "tile-x" : "tile-o";
-      
-      tile.innerHTML = `<span class="${tileType}">${game.board[i] ? game.board[i] : ""}</span>`
+    if (winningCombination) {
+      let winner = game.turn.toLowerCase();
+      document.querySelector(
+        `.player-${winner}`
+      ).innerHTML = `<span>Player ${game.turn}</span>
+          <div class="confetti">
+            <img src="./confetti.png" alt="" />
+          </div>`;
+    } else {
+      document.querySelector(".player-x").innerHTML = "<span>Player X</span>";
+      document.querySelector(".player-o").innerHTML = "<span>Player O</span>";
+    }
 
-      if(winningCombination && winningCombination.includes(i)){
+    for (let i = 0; i < game.board.length; i++) {
+      const tile = document.querySelector(`.board-tile[data-index='${i}']`);
+
+      tile.classList.remove("tile-winner");
+
+      let tileType = game.board[i] === "X" ? "tile-x" : "tile-o";
+
+      // dark mode
+      if (mode !== "light") {
+        tileType = tileType.concat("-dark");
+
+        // use className attribute to replace all existing classes with one or more new classes
+        document.getElementById("restart").className = "restart-dark";
+
+        for (let i = 0; i < 9; i++) {
+          document.getElementById(`board-tile-${i}`).style.background =
+            "#292c35";
+          document
+            .getElementById(`board-tile-${i}`)
+            .classList.add("board-tile-dark");
+        }
+      } else {
+        document.getElementById("restart").className = "restart";
+
+        for (let i = 0; i < 9; i++) {
+          document.getElementById(`board-tile-${i}`).style.background = "white";
+          document
+            .getElementById(`board-tile-${i}`)
+            .classList.remove("board-tile-dark");
+        }
+      }
+
+      tile.innerHTML = `<span class="${tileType}">${
+        game.board[i] ? game.board[i] : ""
+      }</span>`;
+
+      if (winningCombination && winningCombination.includes(i)) {
         tile.classList.add("tile-winner");
       }
     }
   }
 
-  updateTurn(game){
+  updateTurn(game, mode) {
     let playerX = document.querySelector(".player-x");
     let playerO = document.querySelector(".player-o");
-    playerX.classList.remove('active');
-    playerO.classList.remove('active');
-    
-    if(game.turn == 'X') {
-      playerX.classList.add('active');
+    playerX.classList.remove("active");
+    playerO.classList.remove("active");
+    playerX.classList.remove("active-dark");
+    playerO.classList.remove("active-dark");
+
+    if (mode === "light") {
+      if (game.turn == "X") {
+        playerX.classList.add("active");
+      } else {
+        playerO.classList.add("active");
+      }
     } else {
-      playerO.classList.add('active');
+      if (game.turn == "X") {
+        playerX.classList.add("active-dark");
+      } else {
+        playerO.classList.add("active-dark");
+      }
     }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
-import Game from "./Game.js"
-import GameView from "./GameView.js"
+import Game from "./Game.js";
+import GameView from "./GameView.js";
+
+const checkbox = document.getElementById("checkbox");
+
+let mode = "light";
+
+checkbox.addEventListener("change", () => {
+  // change the theme of the website
+  document.body.classList.toggle("dark");
+  mode = mode === "light" ? "dark" : "light";
+  gameView.updateBoard(game, mode);
+});
 
 let game = new Game();
 let gameView = new GameView();
 
 document.querySelector(".restart").addEventListener("click", () => {
   onRestartClick();
-})
-
-let tiles = document.querySelectorAll(".board-tile");
-tiles.forEach(tile => {
-  tile.addEventListener("click", () => {
-    onTileClick(tile.dataset.index);
-  })
 });
 
-function onTileClick(i){
+let tiles = document.querySelectorAll(".board-tile");
+tiles.forEach((tile) => {
+  tile.addEventListener("click", () => {
+    onTileClick(tile.dataset.index);
+  });
+});
+
+function onTileClick(i) {
   // make a move
   game.makeMove(i);
   // update board
-  gameView.updateBoard(game);
+  gameView.updateBoard(game, mode);
 }
 
 function onRestartClick() {
   game = new Game();
-  gameView.updateBoard(game);
+  gameView.updateBoard(game, mode);
 }
 
-gameView.updateBoard(game);
+gameView.updateBoard(game, mode);
